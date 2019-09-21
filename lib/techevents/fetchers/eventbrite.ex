e7ca35c8460cba753cdd227e589009921e721969
sigu <sigumagwa@gmail.com>
2019-09-21 12:33:49 +0300

    add tesla

---
+++ lib/techevents/fetchers/eventbrite.ex
@@ -0,0 +1,14 @@
+defmodule Techevents.Fetchers.Eventbrite do
+  @moduledoc """
+  Send requests out to eventbrite
+  """
+  use Tesla
+  plug Tesla.Middleware.BaseUrl, "https://www.eventbriteapi.com/v3"
+  plug Tesla.Middleware.Headers, [{"authorization", "Bearer #{Application.get_env(:techevents, :eventbrite_token)}"}]
+  plug Tesla.Middleware.JSON
+
+  def search_events do
+    get("/events/search/")
+  end
+
+end
