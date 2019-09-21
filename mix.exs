8485170e537cb21045ef307cd910181ead1e853a
sigu <sigu@podiihq.com>
2019-09-21 09:18:45 +0300

    add credo and lint checker

--- mix.exs
+++ mix.exs
@@ -42,6 +42,7 @@ defmodule Techevents.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
       {:plug_cowboy, "~> 2.0"}
     ]
   end
